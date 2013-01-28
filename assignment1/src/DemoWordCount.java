2002659c1dba892c7148129029b2a29de098abcb
cloudera <cloudera@localhost.localdomain>
2013-01-28 15:14:28 -0500

    Classes, jars, java files added

--- assignment1/src/DemoWordCount.java
+++ assignment1/src/DemoWordCount.java
@@ -1,18 +1,18 @@
 /*
- * Cloud9: A MapReduce Library for Hadoop
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you
- * may not use this file except in compliance with the License. You may
- * obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0 
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+* Cloud9: A MapReduce Library for Hadoop
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you
+* may not use this file except in compliance with the License. You may
+* obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
 
 
 
@@ -37,127 +37,127 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
 /**
- * <p>
- * Simple word count demo. This Hadoop Tool counts words in flat text file, and
- * takes the following command-line arguments:
- * </p>
- * 
- * <ul>
- * <li>[input-path] input path</li>
- * <li>[output-path] output path</li>
- * <li>[num-reducers] number of reducers</li>
- * </ul>
- * 
- * @author Jimmy Lin
- */
+* <p>
+* Simple word count demo. This Hadoop Tool counts words in flat text file, and
+* takes the following command-line arguments:
+* </p>
+*
+* <ul>
+* <li>[input-path] input path</li>
+* <li>[output-path] output path</li>
+* <li>[num-reducers] number of reducers</li>
+* </ul>
+*
+* @author Jimmy Lin
+*/
 public class DemoWordCount extends Configured implements Tool {
-	private static final Logger sLogger = Logger.getLogger(DemoWordCount.class);
+private static final Logger sLogger = Logger.getLogger(DemoWordCount.class);
 
-	// mapper: emits (token, 1) for every word occurrence
-	private static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+// mapper: emits (token, 1) for every word occurrence
+private static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-		// reuse objects to save overhead of object creation
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
+// reuse objects to save overhead of object creation
+private final static IntWritable one = new IntWritable(1);
+private Text word = new Text();
 
-		@Override
-		public void map(LongWritable key, Text value, Context context) throws IOException,
-				InterruptedException {
-			String line = ((Text) value).toString();
-			StringTokenizer itr = new StringTokenizer(line);
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				if(word.matches("[A-Za-z]+")){
-					context.write(word, one);
-				}
-			}
-		}
-	}
+@Override
+public void map(LongWritable key, Text value, Context context) throws IOException,
+InterruptedException {
+String line = ((Text) value).toString();
+StringTokenizer itr = new StringTokenizer(line);
+while (itr.hasMoreTokens()) {
+word.set(itr.nextToken());
+if(word.toString().matches("[A-Za-z]+")){
+context.write(word, one);
+}
+}
+}
+}
 
-	// reducer: sums up all the counts
-	private static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+// reducer: sums up all the counts
+private static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-		// reuse objects
-		private final static IntWritable SumValue = new IntWritable();
+// reuse objects
+private final static IntWritable SumValue = new IntWritable();
 
-		@Override
-		public void reduce(Text key, Iterable<IntWritable> values,
-				Context context) throws IOException, InterruptedException {
-			// sum up values
-			Iterator<IntWritable> iter = values.iterator();
-			int sum = 0;
-			while (iter.hasNext()) {
-				sum += iter.next().get();
-			}
-			SumValue.set(sum);
-			context.write(key, SumValue);
-		}
-	}
+@Override
+public void reduce(Text key, Iterable<IntWritable> values,
+Context context) throws IOException, InterruptedException {
+// sum up values
+Iterator<IntWritable> iter = values.iterator();
+int sum = 0;
+while (iter.hasNext()) {
+sum += iter.next().get();
+}
+SumValue.set(sum);
+context.write(key, SumValue);
+}
+}
 
-	/**
-	 * Creates an instance of this tool.
-	 */
-	public DemoWordCount() {
-	}
+/**
+* Creates an instance of this tool.
+*/
+public DemoWordCount() {
+}
 
-	private static int printUsage() {
-		System.out.println("usage: [input-path] [output-path] [num-reducers]");
-		ToolRunner.printGenericCommandUsage(System.out);
-		return -1;
-	}
+private static int printUsage() {
+System.out.println("usage: [input-path] [output-path] [num-reducers]");
+ToolRunner.printGenericCommandUsage(System.out);
+return -1;
+}
 
-	/**
-	 * Runs this tool.
-	 */
-	public int run(String[] args) throws Exception {
-		if (args.length != 3) {
-			printUsage();
-			return -1;
-		}
+/**
+* Runs this tool.
+*/
+public int run(String[] args) throws Exception {
+if (args.length != 3) {
+printUsage();
+return -1;
+}
 
-		String inputPath = args[0];
-		String outputPath = args[1];
-		int reduceTasks = Integer.parseInt(args[2]);
+String inputPath = args[0];
+String outputPath = args[1];
+int reduceTasks = Integer.parseInt(args[2]);
 
-		sLogger.info("Tool: DemoWordCount");
-		sLogger.info(" - input path: " + inputPath);
-		sLogger.info(" - output path: " + outputPath);
-		sLogger.info(" - number of reducers: " + reduceTasks);
+sLogger.info("Tool: DemoWordCount");
+sLogger.info(" - input path: " + inputPath);
+sLogger.info(" - output path: " + outputPath);
+sLogger.info(" - number of reducers: " + reduceTasks);
 
-		Configuration conf = new Configuration();
-		Job job = new Job(conf, "DemoWordCount");
-		job.setJarByClass(DemoWordCount.class);
+Configuration conf = new Configuration();
+Job job = new Job(conf, "DemoWordCount");
+job.setJarByClass(DemoWordCount.class);
 
-		job.setNumReduceTasks(reduceTasks);
+job.setNumReduceTasks(reduceTasks);
 
-		FileInputFormat.setInputPaths(job, new Path(inputPath));
-		FileOutputFormat.setOutputPath(job, new Path(outputPath));
+FileInputFormat.setInputPaths(job, new Path(inputPath));
+FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+job.setOutputKeyClass(Text.class);
+job.setOutputValueClass(IntWritable.class);
 
-		job.setMapperClass(MyMapper.class);
-		job.setCombinerClass(MyReducer.class);
-		job.setReducerClass(MyReducer.class);
+job.setMapperClass(MyMapper.class);
+job.setCombinerClass(MyReducer.class);
+job.setReducerClass(MyReducer.class);
 
-		// Delete the output directory if it exists already
-		Path outputDir = new Path(outputPath);
-		FileSystem.get(conf).delete(outputDir, true);
+// Delete the output directory if it exists already
+Path outputDir = new Path(outputPath);
+FileSystem.get(conf).delete(outputDir, true);
 
-		long startTime = System.currentTimeMillis();
-		job.waitForCompletion(true);
-		sLogger.info("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0
-				+ " seconds");
+long startTime = System.currentTimeMillis();
+job.waitForCompletion(true);
+sLogger.info("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0
++ " seconds");
 
-		return 0;
-	}
+return 0;
+}
 
-	/**
-	 * Dispatches command-line arguments to the tool via the
-	 * <code>ToolRunner</code>.
-	 */
-	public static void main(String[] args) throws Exception {
-		int res = ToolRunner.run(new Configuration(), new DemoWordCount(), args);
-		System.exit(res);
-	}
+/**
+* Dispatches command-line arguments to the tool via the
+* <code>ToolRunner</code>.
+*/
+public static void main(String[] args) throws Exception {
+int res = ToolRunner.run(new Configuration(), new DemoWordCount(), args);
+System.exit(res);
+}
 }
