c94ed100e07c44f82684eae1833dbcf9572b0185
kentwills <kentwills@aol.com>
2013-01-28 22:13:29 -0500

    refactored for IVY

--- assignment1/run-assignment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p src/wordcount_classes
-javac -classpath src/hadoop-core-2.0.0-mr1-cdh4.1.1.jar:src/hadoop-common-2.0.0-cdh4.1.1.jar:src/log4j-1.2.17.jar -d src/wordcount_classes src/DemoWordCount.java
-jar -cvf ~/wordcount.jar -C src/wordcount_classes/ .
-hadoop jar src/wordcount.jar \ -input bible+shakes.nopunc.gz -output wc -numReducers 5
