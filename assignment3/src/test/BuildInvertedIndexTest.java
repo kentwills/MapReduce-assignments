c901e91c7657d933b75e4bcb74be9ec90ff8b24c
Kent Wills <kentwills@aol.com>
2013-02-24 18:11:48 -0500

    don't think this will work

--- assignment3/src/test/BuildInvertedIndexTest.java
+++ assignment3/src/test/BuildInvertedIndexTest.java
@@ -4,7 +4,7 @@ package test;
 import static org.mockito.Mockito.*;
 import java.io.IOException;
 import org.apache.hadoop.io.*;
-import main.BuildInvertedIndex.*;
+import main.BuildInvertedIndex;
 
 //import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -17,11 +17,11 @@ public class BuildInvertedIndexTest {
 	@Test
 	public void test() throws IOException, InterruptedException{
 		//Create Mapper
-		MyMapper mapper = new MyMapper();
+		BuildInvertedIndex.MyMapper mapper = new BuildInvertedIndex.MyMapper();
 		//Input
 		Text value = new Text("test");
 		//Create context
-		MyMapper.Context context =	mock(MyMapper.Context.class);
+		BuildInvertedIndex.MyMapper.Context context =	mock(BuildInvertedIndex.MyMapper.Context.class);
 		mapper.map(null, value, context);
 		//Check output
 		verify(context).write(new Text("test"), new PairOfInts(1,1));		
