3fb189b60d87187a1debb6a21292ff2a25ee3e0f
Kent Wills <kentwills@aol.com>
2013-02-28 09:17:25 -0500

    output

--- assignment3/src/main/BuildInvertedIndexCompressed.java
+++ assignment3/src/main/BuildInvertedIndexCompressed.java
@@ -44,6 +44,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MapFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
@@ -227,7 +228,7 @@ public class BuildInvertedIndexCompressed extends Configured implements Tool {
 		job.setMapOutputValueClass(IntWritable.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(BytesWritable.class);
-		job.setOutputFormatClass(MapFileOutputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
 		job.setMapperClass(MyMapper.class);
 		job.setReducerClass(MyReducer.class);
