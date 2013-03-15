28c1670210d34f19dadd5e0af1a06a1bafee6487
Kent Wills <kentwills@aol.com>
2013-03-15 14:09:26 -0400

    change print

--- assignment4/src/main/BuildPersonalizedPageRankRecords.java
+++ assignment4/src/main/BuildPersonalizedPageRankRecords.java
@@ -106,9 +106,9 @@ public class BuildPersonalizedPageRankRecords extends Configured implements	Tool
 			}
 
 			for (int i = 0; i < sources.length; i++) {
-				if (Integer.toString(node.getNodeId()).equals(sources[i])) {
-					LOG.info(nid + " " + node.getPageRank(i)+" "+i);
+				if (Integer.toString(node.getNodeId()).equals(sources[i])) {					
 					node.setPageRank((float) 0, i);// Log(1)
+					LOG.info(nid + " " + node.getPageRank(i)+" "+i);
 				} else
 					node.setPageRank((float) Float.NEGATIVE_INFINITY, i);// (Log(0)
 
